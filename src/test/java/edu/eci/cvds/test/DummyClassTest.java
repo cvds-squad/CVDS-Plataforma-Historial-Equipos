ac9d87b0138ead46df07b3a7909d12de4932a486
Julian <julian.benitez@mail.escuelaing.edu.co>
2019-04-07 20:01:54 -0500

    setup del proyecto

---
+++ src/test/java/edu/eci/cvds/test/DummyClassTest.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class DummyClassTest {
+
+    @Test
+    public void alwaysTrue(){
+        assertTrue(true);
+    }
+
+}
