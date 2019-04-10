5244d246c5f9494f4fcf8978b1d92575e09e5c48
Andres Quintero <andres.quintero98@hotmail.com>
2019-04-10 17:35:06 -0500

    llaves primarias y foraneas en tablas bd correccion sintaxis

--- tables.sql
+++ tables.sql
@@ -1,20 +1,31 @@
+create table rolesUsuario(
+idRol int(5) primary key,
+descripcion varchar(500),
+nombre varchar(30));
+
+create table TipoElemento(
+idTElemento int(5) primary key,
+Tipo varchar(100),
+Descripción varchar(100));
+
+
 create table Usuarios(
-nombre varchar(30) notnull,
+nombre varchar(30) not null,
 correo varchar(30) primary key,
 userrol int(5) references rolesUsuario(idRol),
-contraseña varchar (30) notnull,
-estado varchar(30) notnull);
+contraseña varchar (30) not null,
+estado varchar(30) not null);
 
 create table Laboratorios(
 idLab int(5) primary key,
-nombre varchar(30) notnull,
+nombre varchar(30) not null,
 usuario varchar(30) references Usuarios(correo),
 descripcion varchar(500));
 
 create table Equipos(
 idEquipo int(5) primary key,
 labs int (5) references Laboratorios(idLab),
-Novedades varchar(500);
+Novedades varchar(500));
 
 create table Elementos(
 idElemento int(5) primary key,
@@ -22,14 +33,3 @@ eqps int(5) references Equipos(idEquipo),
 tipelement int(5) references TipoElemento(idTElemento),
 marca varchar(20),
 Descripcion varchar(500));
-
-create table TipoElemento(
-idTElemento int(5) primary key,
-Tipo varchar(100),
-Descripción varchar(100));
-
-create table rolesUsuario(
-idRol int(5) primary key,
-descripcion varchar(500),
-nombre varchar(30));
-)
