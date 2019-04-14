bb8751bdb315184bc45514138f2733370a8800ae
Julian <julian.benitez@mail.escuelaing.edu.co>
2019-04-14 14:29:17 -0500

    Front end registro elemento terminado, registro con base de produccion

--- src/main/java/edu/eci/cvds/beans/ElementoBean.java
+++ src/main/java/edu/eci/cvds/beans/ElementoBean.java
@@ -1,11 +1,17 @@
 package edu.eci.cvds.beans;
 
 import com.google.inject.Inject;
+import edu.eci.cvds.samples.entities.Elemento;
 import edu.eci.cvds.samples.entities.TipoElemento;
 import edu.eci.cvds.samples.services.HistoryService;
+import edu.eci.cvds.samples.services.HistoryServiceException;
+import edu.eci.cvds.samples.services.HistoryServicesFactory;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
 
 @ManagedBean(name="elementoBean")
 @RequestScoped
@@ -21,11 +27,11 @@ public class ElementoBean {
     private String descripcion;
 
     public ElementoBean(){
-
+        historyService = HistoryServicesFactory.getInstance().getHistoryService();
     }
 
-    public void setTipoElemento(String tipoElemento){
-        this.tipoElemento = TipoElemento.valueOf(tipoElemento);
+    public void setTipoElemento(TipoElemento tipoElemento){
+        this.tipoElemento = tipoElemento;
     }
 
     public TipoElemento getTipoElemento(){
@@ -50,5 +56,21 @@ public class ElementoBean {
 
     public TipoElemento[] tipos(){
         return TipoElemento.values();
+    }
+
+
+    public void registro() {
+        Elemento elemento = new Elemento(tipoElemento,marca,descripcion);
+        String msg;
+        try {
+            historyService.registrarElemento(elemento);
+            msg = "Elemento registrado correctamente";
+            setDescripcion(null);
+            setMarca(null);
+            setTipoElemento(null);
+        } catch (HistoryServiceException e) {
+            msg = "Fallo al registrar el elemento";
+        }
+        FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,msg,msg));
     }
 }
