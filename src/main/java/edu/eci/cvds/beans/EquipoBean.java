c7f7e79d3fde0329cfeb4e89a5f43bca7b036ae6
Julian <julian.benitez@mail.escuelaing.edu.co>
2019-04-14 22:53:07 -0500

    Front de asociar elemento a equipo

--- src/main/java/edu/eci/cvds/beans/EquipoBean.java
+++ src/main/java/edu/eci/cvds/beans/EquipoBean.java
@@ -1,23 +1,28 @@
 package edu.eci.cvds.beans;
 
+import edu.eci.cvds.samples.entities.Elemento;
 import edu.eci.cvds.samples.entities.Equipo;
+import edu.eci.cvds.samples.entities.TipoElemento;
 import edu.eci.cvds.samples.services.HistoryService;
 import edu.eci.cvds.samples.services.HistoryServiceException;
 import edu.eci.cvds.samples.services.HistoryServicesFactory;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import java.io.Serializable;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @ManagedBean(name="equipoBean")
-@RequestScoped
-public class EquipoBean {
+@SessionScoped
+public class EquipoBean implements Serializable {
 
     private HistoryService historyService;
 
-    private int idEquipo;
+    private Equipo equipo;
 
     public EquipoBean(){
         historyService = HistoryServicesFactory.getInstance().getHistoryService();
@@ -33,18 +38,33 @@ public class EquipoBean {
         return equipos;
     }
 
+    public void asociar(List<Elemento> elementos){
+        Set<TipoElemento> set = new HashSet<>();
+        for (Elemento elemento : elementos){
+            TipoElemento tipoElemento = elemento.getTipelement();
+            if (!set.contains(tipoElemento)) set.add(elemento.getTipelement());
+            else{
+                FacesContext.getCurrentInstance().addMessage("msgsElemento",new FacesMessage(FacesMessage.SEVERITY_ERROR,"No puede asociar dos elementos del mismo tipo","No puede asociar dos elementos del mismo tipo"));
+                return;
+            }
+        }
 
-    public void setIdEquipo(int idEquipo) {
-        this.idEquipo = idEquipo;
+        for (Elemento elemento : elementos){
+            try{
+                historyService.asociarElementoConEquipo(equipo.getIdEquipo(),elemento.getIdElemento());
+            }catch (HistoryServiceException e){
+                FacesContext.getCurrentInstance().addMessage("msgsElemento",new FacesMessage(FacesMessage.SEVERITY_ERROR,"Error al asociar elemento","Error al asociar elemento"));
+                return;
+            }
+        }
+        FacesContext.getCurrentInstance().addMessage("msgsElemento",new FacesMessage(FacesMessage.SEVERITY_INFO,"Elementos asociados correctamente","Elementos asociados correctamente"));
     }
 
-    public void asociar(int idElemento){
-        System.out.println(idElemento + " "+ idEquipo);
-        try {
-            historyService.asociarElementoConEquipo(idEquipo,idElemento);
-            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_INFO,"Elemento asociado correctamente", "Elemento asociado correctamente"));
-        } catch (HistoryServiceException e) {
-            FacesContext.getCurrentInstance().addMessage(null,new FacesMessage(FacesMessage.SEVERITY_ERROR,"Error al asociar", "Error al asociar"));
-        }
+    public Equipo getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(Equipo equipo) {
+        this.equipo = equipo;
     }
 }
