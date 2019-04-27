8cc3509f132b8f895e3e72bbb6905486fc9fa699
Julian <julian.benitez@mail.escuelaing.edu.co>
2019-04-27 00:28:07 -0500

    Front registro novedad Elemento falta registro novedad Equipo

--- src/main/java/edu/eci/cvds/beans/NovedadBean.java
+++ src/main/java/edu/eci/cvds/beans/NovedadBean.java
@@ -2,9 +2,11 @@ package edu.eci.cvds.beans;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
 
 @ManagedBean
-@RequestScoped
-public class NovedadBean {
+@SessionScoped
+public class NovedadBean implements Serializable {
 
 }
